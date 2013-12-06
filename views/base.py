39a58f392d3003391ab53b3d8aafb813b7f63b74
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-12-06 11:26:10 -0200

    Implementa paginação nos problemas
    fixes #19

--- views/base.py
+++ views/base.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator
 
 from dnstorm.models import Problem
 from dnstorm.forms import OptionsForm, AccountCreateForm
@@ -15,7 +16,10 @@ class HomeView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['problems'] = Problem.objects.all()
+        problems = Paginator(Problem.objects.all(), 25)
+        page = self.request.GET['page'] if 'page' in self.request.GET else 1
+        context['problems'] = problems.page(page)
+        context['problems_total'] = Problem.objects.all().count()
         return context
 
 class OptionsView(FormView):
