2203ce3cdbf392613a266a5a23c6feb23d59e5fa
Vinicius Massuchetto <vmassuchetto@gmail.com>
2014-01-14 23:36:23 -0200

    Using other context variables for user profile

--- dnstorm/app/views/base.py
+++ dnstorm/app/views/base.py
@@ -59,19 +59,18 @@ class UserView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(UserView, self).get_context_data(**kwargs)
-        self.user = get_object_or_404(User, username=kwargs['username'])
-        context['user'] = self.user
-        context['breadcrumbs'] = self.get_breadcrumbs()
-        context['activities'] = ActivityManager().get_objects(user=self.user.id, limit=20)
-        context['problem_count'] = Problem.objects.filter(author=self.user).count()
-        context['idea_count'] = Idea.objects.filter(author=self.user).count()
-        context['comment_count'] = Comment.objects.filter(author=self.user).count()
+        context['profile'] = get_object_or_404(User, username=kwargs['username'])
+        context['breadcrumbs'] = self.get_breadcrumbs(username=context['username'])
+        context['activities'] = ActivityManager().get_objects(user=context['profile'].id, limit=20)
+        context['problem_count'] = Problem.objects.filter(author=context['profile']).count()
+        context['idea_count'] = Idea.objects.filter(author=context['profile']).count()
+        context['comment_count'] = Comment.objects.filter(author=context['profile']).count()
         return context
 
-    def get_breadcrumbs(self):
+    def get_breadcrumbs(self, **kwargs):
         return [
             { 'title': _('Users'), 'classes': 'unavailable' },
-            { 'title': self.user.username, 'classes': 'current' } ]
+            { 'title': kwargs['username'], 'classes': 'current' } ]
 
 class CommentView(RedirectView):
     permanent = True
