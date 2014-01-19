9567a456542a7d3253b6c04cf93a2f450425d419
Vinicius Massuchetto <vmassuchetto@gmail.com>
2014-01-18 22:39:06 -0200

    Loading settings from Django's global_settings

--- dnstorm/settings.py
+++ dnstorm/settings.py
@@ -1,14 +1,13 @@
-# Django settings for dnstorm project.
 import os
 import dj_database_url
 
+from django.conf import global_settings
+
+# Local development environment
+
 LOCALENV = False
 
-ADMINS = (
-    ('Vinicius Massuchetto', 'vmassuchetto@gmail.com'),
-)
-
-MANAGERS = ADMINS
+# Databases
 
 if LOCALENV:
     DEBUG = True
@@ -23,20 +22,19 @@ else :
     DEBUG = False
     DATABASES = { 'default': dj_database_url.config() }
 
+# Django settings
+
+ADMINS = (
+    ('Vinicius Massuchetto', 'vmassuchetto@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
 TEMPLATE_DEBUG = DEBUG
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'pt-br'
 LANGUAGES = (
   ('en', 'English'),
@@ -44,97 +42,31 @@ LANGUAGES = (
 )
 
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = SITE_ROOT + '/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
 STATIC_ROOT = 'static'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '+=hz$82m0-sh@+=i64h-+i%8_3+m=w2(^hf2bnha+v&6)^-qd^'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
+MIDDLEWARE_CLASSES = global_settings.MIDDLEWARE_CLASSES + (
     'reversion.middleware.RevisionMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.request',
-    'absolute.context_processors.absolute',
-    'dnstorm.app.context_processors.base'
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'dnstorm.app.context_processors.base',
 )
 
 ROOT_URLCONF = 'dnstorm.app.urls'
 LOGIN_REDIRECT_URL = '/'
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'dnstorm.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+# Apps
 
 INSTALLED_APPS = (
     # Built-in apps included by default
@@ -148,11 +80,10 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
 
     # Third-party apps
-    'absolute',
+    'avatar',
     'ckeditor',
     'crispy_forms',
     'crispy_forms_foundation',
-    'avatar',
     'haystack',
     'registration',
     'reversion',
@@ -165,11 +96,8 @@ INSTALLED_APPS = (
 if LOCALENV:
     INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# Logging
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -199,7 +127,7 @@ LOGGING = {
 DATE_FORMAT = '%d %b %Y'
 DATETIME_FORMAT = '%d %b %Y H:i'
 
-# DNStorm
+# DNStorm settings
 
 DNSTORM = {
     'table_prefix': 'dnstorm',
@@ -209,6 +137,7 @@ DNSTORM = {
 }
 
 # SMTP
+
 EMAIL_HOST = False
 EMAIL_PORT = False
 EMAIL_HOST_USER = 'dnstorm@localhost'
