6646174435134b7ebd8d601c4ca1b89f1c8425cd
Vinicius Massuchetto <vmassuchetto@gmail.com>
2015-02-25 22:47:07 -0300

    Fixing error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements(<requirements_path>)
+install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 metadata = {
